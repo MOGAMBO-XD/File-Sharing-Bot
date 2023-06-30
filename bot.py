a7735a6fbbe05496c02ea62a49ac9bb2046614d5
MOGAMBO <93328169+MOGAMBO-XD@users.noreply.github.com>
2023-06-30 17:28:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,7 +55,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CYBERMUSICPROJECT")
         self.LOGGER(__name__).info(f""" \n\n       
         MOGAMBO 
                                           """)
